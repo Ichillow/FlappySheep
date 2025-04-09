1b93876cc9bbb09a3e6dbd29a3f56447b489f9e1
Loïs Maytraud <lois.maytraud@gmail.com>
2025-04-10 00:33:45 +0200

    Ajout des assets pour le menu

---
+++ Scripts/UI/menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+
+func _on_texture_button_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_texture_button_option_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_texture_button_quit_pressed() -> void:
+	pass # Replace with function body.
--- Scripts/barrier.gd
+++ Scripts/barrier.gd
@@ -19,19 +19,19 @@ func _process(delta):
 		queue_free()
 
 
-func _on_kill_1_body_entered(other_body) -> void:
-	if other_body.name == "sheep":
-		var death_timer = other_body.get_node("deathTimer")
+func _on_kill_1_body_entered(body) -> void:
+	if body.name == "sheep":
+		var death_timer = body.get_node("deathTimer")
 		if death_timer:
-			other_body.can_jump = false  # Désactive le saut
+			body.can_jump = false  # Désactive le saut
 			death_timer.start(1)
 			death_timer.timeout.connect(_on_death_timer_timeout, CONNECT_ONE_SHOT)
 
-func _on_kill_2_body_entered(other_body) -> void:
-	if other_body.name == "sheep":
-		var death_timer = other_body.get_node("deathTimer")
+func _on_kill_2_body_entered(body) -> void:
+	if body.name == "sheep":
+		var death_timer = body.get_node("deathTimer")
 		if death_timer:
-			other_body.can_jump = false  # Désactive le saut
+			body.can_jump = false  # Désactive le saut
 			death_timer.start(1)
 			death_timer.timeout.connect(_on_death_timer_timeout, CONNECT_ONE_SHOT)
 
